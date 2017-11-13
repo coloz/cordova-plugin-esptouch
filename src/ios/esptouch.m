29dd91d321eb90eb18766135bc1179b8199d4545
unknown <clz@clz.me>
2017-11-13 15:08:44 +0800

    xcode自动链接.a库; 修正函数名错误;

--- src/ios/esptouch.m
+++ src/ios/esptouch.m
@@ -10,12 +10,11 @@
 
 -(void) onEsptouchResultAddedWithResult: (ESPTouchResult *) result
 {
-    NSString *InetAddress=[ESP_NetUtil descriptionInetAddrByData:result.ipAddrData];
+    NSString *InetAddress=[ESP_NetUtil descriptionInetAddr4ByData:result.ipAddrData];
     NSString *text=[NSString stringWithFormat:@"bssid=%@,InetAddress=%@",result.bssid,InetAddress];
     CDVPluginResult* pluginResult = nil;
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: text];
     [pluginResult setKeepCallbackAsBool:true];
-    //[self.commandDelegate sendPluginResult:pluginResult callbackId:self.command.callbackId];  //add by lianghuiyuan
 }
 @end
 
